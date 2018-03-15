77b48e68acbf8e106fa06042bbc007171cd95115
Jaan Pullerits <clysuva@gmail.com>
2018-03-16 00:41:09 +0200

    Do not pass too much out of the abstraction - Makes it easier to replace in the future.

--- src/MidiController.js
+++ src/MidiController.js
@@ -8,13 +8,13 @@ WebMidi.enable(() => {
     const input = WebMidi.getInputByName("TSCFF");
 
     input.on("noteon", "all", (noteEvent) => {
-        let { note } = noteEvent;
+        let { note, velocity, channel } = noteEvent;
         let noteName = note.name + note.octave;
 
         console.log(noteName, noteEvent)
 
         if(noteListeners[note]) {
-            noteListeners[note].forEach((cb) => cb(noteEvent))
+            noteListeners[note].forEach((cb) => cb({note, velocity, channel}))
         }
     })
 })
