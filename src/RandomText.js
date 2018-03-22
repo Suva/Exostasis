0c30c8eb19bc6f7c890621a309f7d151000d6a36
Jaan Pullerits <jaan.pullerits@bigbank.ee>
2018-03-22 15:58:48 +0200

    Maek blurry

--- src/RandomText.js
+++ src/RandomText.js
@@ -3,8 +3,12 @@ module.exports = function (ctx) {
         render() {
             ctx.globalCompositeOperation = 'screen'
             ctx.fillStyle = "white"
-            ctx.font = '180px murder'
-            ctx.fillText('test', Math.random() * 1920, Math.random() * 1080)
+            ctx.filter = "blur(3px)"
+            for(let i = 0; i < 10; i++) {
+                ctx.font = Math.random() * 300 + 'px murder'
+                ctx.fillText('test', Math.random() * 1920, Math.random() * 1080)
+            }
+            ctx.filter = "none"
             ctx.globalCompositeOperation = 'source-over'
             ctx.setTransform(1, 0, 0, 1, 0, 0);
         }
